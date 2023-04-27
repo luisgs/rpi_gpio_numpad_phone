99e53963e65bea67d7c760d6e84b6ed714fd02a3
luisgs <luis.ild@gmail.com>
2023-04-27 23:16:51 +0200

    functions and all together

--- func_keypad.py
+++ func_keypad.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
+import lineButton
+
 import logging, sys
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
@@ -27,17 +29,6 @@ C3E = 11    # physical matrix has an additional cable
 C4 = 25
 
 
-# Step in time
-step_in_time = 0.1
-
-# Waiting time (#steps)
-waiting_time = 30
-
-# treshold
-treshold = 0
-
-# FINAL CODE (string!)
-code = ""
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -72,9 +63,24 @@ def readLine(line, characters):
   GPIO.output(line, GPIO.LOW)
   return digit
 
-try:
+
+def read_phone_number():
+  # Step in time
+  # seconds waiting to read all GPIO numpad ports
+  step_in_time = 0.1
+
+  # Waiting time (#steps)
+  # time waiting to send out read code
+  waiting_time = 30
+
+  # treshold
+  treshold = 0
+
+  # FINAL CODE (string!)
+  # Code that will be send out
+  code = ""
   ori_code = code
-  while True:
+  while lineButton.readLine():
     code += readLine(L1 ,["1", "2", "3", "A"])
     code += readLine(L2 ,["4", "5", "6", "B"])
     code += readLine(L3 ,["7", "8", "9", "C"])
@@ -82,20 +88,35 @@ try:
 
     if (waiting_time == treshold) and code:
       logging.info("CODE IS FOUND:" + code)
-      code = ""
-      break 
+      return code
     else:
       treshold = treshold % waiting_time
 
     if ori_code != code:
       treshold = 0
       ori_code = code
+      logging.info("num is typed in:" + code)
     
     treshold += 1 
 
     logging.info("treshold:" + str(treshold))
-    logging.info("code is:" + code)
     time.sleep(step_in_time)
+  
+  return code
+
+try:
+  while True:
+#    logging.info("CODE: " + read_phone_number())
+    logging.info("hola")
+    if lineButton.readLine():
+      code = read_phone_number()
+      logging.info("PHONE is UP")
+      logging.info("We read this CODE:" + code)
+    else:
+      logging.info("PHONE is DOWN")
+
+    time.sleep(0.1)
+
 
 except KeyboardInterrupt:
-    logging.info("Application stopped!")
+    logging.error("Application stopped!")
--- lineButton.py
+++ lineButton.py
@@ -1,37 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
+import logging, sys
 
-B1 = 7
-B2 = 8
-B3 = 9
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 
-status = ["PICKED_UP", "HUNG_UP"]
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+def readLine():
+  B1 = 7
+  B2 = 8
+  B3 = 9
 
-GPIO.setup(B1, GPIO.OUT)
-#GPIO.setup(B2, GPIO.OUT)
-#GPIO.setup(B3, GPIO.OUT)
+  status = ["PICKED_UP", "HUNG_UP"]
 
-#GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(B2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)
 
-def readLine(line, status):
-  GPIO.output(line, GPIO.HIGH)
+  GPIO.setup(B1, GPIO.OUT)
+  #GPIO.setup(B2, GPIO.OUT)
+  #GPIO.setup(B3, GPIO.OUT)
+
+  #GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setup(B2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  #GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+  GPIO.output(B1, GPIO.HIGH)
   if (GPIO.input(B2) == 0):
-    print(status[0])
+    logging.info(status[0])
+    return True
   if (GPIO.input(B2) == 1):
-    print(status[1])
-  GPIO.output(line, GPIO.LOW)
+    logging.info(status[1])
+    return False
+  GPIO.output(B1, GPIO.LOW)
 
-try:
-  while True:
-#    readLine(B3, status)
-    readLine(B1, status)
-    time.sleep(1)
-except KeyboardInterrupt:
-    print("\nApplication stopped!")
+
+#try:
+#  while True:
+#    readLine()
+#    time.sleep(0.1)
+#except KeyboardInterrupt:
+#    logging.error("Application stopped!")
+    
