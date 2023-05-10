fe3edffc8e200facd85540b1a5ac949be3685319
luisgs <luis.ild@gmail.com>
2023-05-11 01:12:04 +0200

    functional keypad and sounds for keystrokes and singal line

--- func_keypad.py
+++ func_keypad.py
@@ -2,8 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import logging, sys
 import pygame       # play sounds
-from multiprocessing import Process
-
+from threading import Thread
 
 import lineButton   # user libraries
 
@@ -14,17 +13,38 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 # Note: numpad is a matrix (4x4)
 # Note: we are using a RPI B
 
-#
 
-def play_sound(sound_track, options):
-  pygame.mixer.init()
-  pygame.mixer.music.load(sound_track)
-  pygame.mixer.music.set_volume(1)
-  pygame.mixer.music.play(loops=options)
-  logging.info("We ARE PLATying this track: " + sound_track)
-  while pygame.mixer.music.get_busy() == True:
-    continue
-  pygame.mixer.quit()
+# GLOBAL VARIABLES
+signal_tone = "media/audio/phone-signal-tone.mp3"
+busy_line = "media/audio/phone-busy-1.mp3"
+
+# Object for creating sounds
+pygame.mixer.init()
+pygame.mixer.set_num_channels(10)
+
+#
+#   PLAY_SOUND
+#   PLays signal tone or busy line
+def play_sound(option):
+    global pygame
+    global signal_tone
+
+    logging.info("Play_sound")
+
+    while True:
+        if lineButton.readLine():
+            logging.info("Play_sound: PHONE is ON")
+            logging.info("singla tone: " + signal_tone)
+            pygame.mixer.music.load(signal_tone)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy() and lineButton.readLine():
+                continue
+        else:
+            logging.info("Play_sound: PHONE is OFF")
+            pygame.mixer.music.stop()
+
+        logging.info("Play_sound: looping back")
 
 
 # GPIO ports for reading
@@ -58,98 +78,120 @@ GPIO.setup(C3E, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def readLine(line, characters):
-  GPIO.output(line, GPIO.HIGH)
-  digit = ""
-  if (GPIO.input(C1) == 1):
-    logging.info(characters[0])
-    digit =  characters[0]
-    play_sound("media/audio/cell-phone-1-nr0.mp3", 1)
-  elif (GPIO.input(C2) == 1):
-    logging.info(characters[1])
-    digit =  characters[1]
-    play_sound("media/audio/cell-phone-1-nr0.mp3", 1)
-  elif ((GPIO.input(C3) == 1) or (GPIO.input(C3E) == 1)):
-      logging.info(characters[2])
-      digit =  characters[2]
-      play_sound("media/audio/cell-phone-1-nr0.mp3", 1)
-  elif (GPIO.input(C4) == 1):
-    logging.info(tcharacters[3])
-    digit =  characters[3]
-    play_sound("media/audio/cell-phone-1-nr0.mp3", 1)
-  
-  GPIO.output(line, GPIO.LOW)
-  return digit
+    global pygame
+    GPIO.output(line, GPIO.HIGH)
+    digit = ""
+    if (GPIO.input(C1) == 1):
+        logging.info(characters[0])
+        digit =  characters[0]
+        pygame.mixer.Channel(2).play(pygame.mixer.Sound("media/audio/cell-phone-1-nr1.mp3"))
+        while pygame.mixer.Channel(2).get_busy():
+            continue
+    elif (GPIO.input(C2) == 1):
+        logging.info(characters[1])
+        digit =  characters[1]
+        pygame.mixer.Channel(2).play(pygame.mixer.Sound("media/audio/cell-phone-1-nr2.mp3"))
+        while pygame.mixer.Channel(2).get_busy():
+            continue
+    elif ((GPIO.input(C3) == 1) or (GPIO.input(C3E) == 1)):
+        logging.info(characters[2])
+        digit =  characters[2]
+        pygame.mixer.Channel(2).play(pygame.mixer.Sound("media/audio/cell-phone-1-nr3.mp3"))
+        while pygame.mixer.Channel(2).get_busy():
+            continue
+    elif (GPIO.input(C4) == 1):
+        logging.info(tcharacters[3])
+        digit =  characters[3]
+        pygame.mixer.Channel(2).play(pygame.mixer.Sound("media/audio/cell-phone-1-nr4.mp3"))
+        while pygame.mixer.Channel(2).get_busy():
+            continue
+
+    GPIO.output(line, GPIO.LOW)
+    return digit
 
 
-def read_phone_number():
-  # Step in time
-  # seconds waiting to read all GPIO numpad ports
-  step_in_time = 0.1
 
-  # Waiting time (#steps)
-  # time waiting to send out read code
-  waiting_time = 30
-
-  # treshold
-  treshold = 0
-
-  # FINAL CODE (string!)
-  # Code that will be send out
-  code = ""
-  ori_code = code
-  while lineButton.readLine():
-#    play_sound("media/audio/phone-signal-tone.mp3")
-    code += readLine(L1 ,["1", "2", "3", "A"])
-    code += readLine(L2 ,["4", "5", "6", "B"])
-    code += readLine(L3 ,["7", "8", "9", "C"])
-    code += readLine(L4 ,["*", "0", "#", "D"])
-
-
-    if (ori_code != code):  # code has been updated!
-      # makenoise(button is clicked)
-      ori_code = code
-      treshold = 0          # start timer again
-    elif (treshold == waiting_time):
-      logging.info("Time is over!")
-      logging.info("CODE is: " + code)
-      return code
-
-#    if (waiting_time == treshold) and code:
-#      logging.info("CODE IS FOUND:" + code)
-#      return code
-#    else:
-#      treshold = treshold % waiting_time
 #
-#    if ori_code != code:
-#      treshold = 0
-#      ori_code = code
-#      logging.info("num is typed in:" + code)
-  
-    treshold += 1 
+#   READ_PHONE_NUMBER
+#   
+#       We read all key strokes frmo keypad during a max time
+#
+def read_phone_number():
+    # Step in time
+    # We read all GPIO every step_in_time seconds
+    step_in_time = 0.01
 
-    logging.info("Treshold++:" + str(treshold))
-    time.sleep(step_in_time)
-  
-  return code
+    # Waiting time (#steps)
+    # time waiting to send out read code
+    waiting_time = 300
+
+    # treshold
+    treshold = 0
+
+    # FINAL CODE (string!)
+    # Code that will be send out
+    code = ""
+    ori_code = code
+
+    # Read while phone is off the hook
+    while lineButton.readLine():
+#    play_sound("media/audio/phone-signal-tone.mp3")
+        code += readLine(L1 ,["1", "2", "3", "A"])
+        code += readLine(L2 ,["4", "5", "6", "B"])
+        code += readLine(L3 ,["7", "8", "9", "C"])
+        code += readLine(L4 ,["*", "0", "#", "D"])
 
 
+        if (ori_code != code):  # code has been updated!
+            # makenoise(button is clicked)
+            ori_code = code
+            treshold = 0          # start timer again
+        if (treshold >= waiting_time):
+            logging.info("Time is over!")
+            logging.info("CODE is: " + code)
+            # RETURN CODE!
+            return code
+
+        treshold += 1 
+
+#        logging.info("Treshold++:" + str(treshold))
+        time.sleep(step_in_time)
+ 
+    # This code shuold not be useful. PHONE is hung up
+    return False 
+
+
+
+#
+#   MAIN
+#
+def main():
+    global signal_tone
+    global busy_line
+    logging.info("MAIN:")
+    # Fire up thread for singal or busy tone sounds
+    thread_sound = Thread(target=play_sound, args=(1, ))
+    thread_sound.start()
+    
+    while True:
+        code = False 
+        if lineButton.readLine():   # phone is picked up!
+            logging.info("MAIN: PHONE is UP")
+            code = read_phone_number()
+            if (not code):
+                logging.info("MAIN: CODE is EMPTY")
+                singal_tone = busy_line
+                logging.info("singla tone: " + signal_tone)
+                logging.info("busy tone: " + busy_line)
+            else:
+                logging.info("MAIN: CODE: " + code)
+        else:
+            logging.info("MAIN: PHONE is DOWN")
 
 try:
-  while True:
-#    logging.info("CODE: " + read_phone_number())
-    if lineButton.readLine():   # phone is picked up!
-      proc_sound = Process(target=play_sound, args=('media/audio/phone-signal-tone.mp3', -1, ))
-      proc_sound.start()
-      code = read_phone_number()
-      logging.info("PHONE is UP")
-      logging.info("We read this CODE:" + code)
-      proc_sound.terminate()
-      proc_sound.join()
-    else:
-      logging.info("PHONE is DOWN")
-
-    time.sleep(0.1)
-
+    if __name__ == "__main__":
+        main()
 
 except KeyboardInterrupt:
     logging.error("Application stopped!")
+    sys.exit()
